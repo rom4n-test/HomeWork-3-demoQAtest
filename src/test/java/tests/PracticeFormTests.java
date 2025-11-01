18f492056465c06941a4c4993320d33672463b62
Roman M. <romanmazoff@gmail.com>
2025-11-01 16:02:32 +0300

    Закончил дз PracticeFormTests.java

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -3,12 +3,15 @@ package tests;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.selector.ByText;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -16,6 +19,8 @@ public class PracticeFormTests {
 
     @BeforeAll
     static void beforeAll() {
+//указываем браузер
+        Configuration.browser = "chrome";
 //устанавливаем разрешение страницы
         Configuration.browserSize = "1920x1080";
 //без этого костыля бесконечная загрузка страницы, победить которую не удалось
@@ -24,34 +29,40 @@ public class PracticeFormTests {
         Configuration.baseUrl = "https://demoqa.com";
 //указываем что не хотим закрывать браузер по завершению
         Configuration.holdBrowserOpen = true;
+//замедлить
+        sleep(500);
     }
 
         @Test
         void fillFormTest() {
 //открываем страницу
             open("/automation-practice-form");
+
 //вводим значениия для полей First Name, Last Name, Email
             $("#firstName").setValue("Valeriy");
             $("#lastName").setValue("Samodurov");
             $("#userEmail").setValue("valeron1337@test.ru");
+
 //проверяем рабату батонов
             $("[for='gender-radio-3']").click();
             $("[for='gender-radio-2']").click();
             $("[for='gender-radio-1']").click();
+
 //водим номер тел
             $("#userNumber").setValue("9993274687");
 
-//вводим дату вручную и проверяем        ЭКСПЕРИМЕНТ НЕ УДАЛСЯ((
-//            $("#dateOfBirthInput").click();
-//            $("#dateOfBirthInput").click();
-//            $("#dateOfBirthInput").sendKeys(Keys.chord(Keys.CONTROL, "a"));
-//            $("#dateOfBirthInput").setValue("01.12.2004");
+//вводим дату вручную и проверяем что она встала по маске
+            $("#dateOfBirthInput").click();
+            $("#dateOfBirthInput").sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            $("#dateOfBirthInput").sendKeys("01.12.2004");
+            $("#dateOfBirthInput").sendKeys(Keys.ENTER);
+            $("#dateOfBirthInput").shouldHave(attribute("value", "12 Jan 2004"));
 
 //вводим дату через пикер
             $("#dateOfBirthInput").click();
             $(".react-datepicker__year-select").selectOptionByValue("1990");
             $(".react-datepicker__month-select").selectOptionByValue("6");
-            $(".react-datepicker__day--019").click();
+            $(".react-datepicker__day--019").hover().click();
 
 //вводим два предмета и удаляем первый
             $("#subjectsContainer").click();
@@ -74,8 +85,32 @@ public class PracticeFormTests {
             File picture = new File("src/test/resources/Приложение.jpeg");
             $("#uploadPicture").uploadFile(picture);
 
-//
+//вводим адрес
             $("#currentAddress").click();
             $("#currentAddress").setValue("Sun street 12");
+
+//выбираем штат и город
+            $("#state").click();
+            $(byText("Uttar Pradesh")).click();
+            $("#city").click();
+            $(byText("Agra")).click();
+
+//кликаем на субмит
+            $("#submit").hover().click();
+
+//проверяем таблицу с введёнными данными
+            $(".table-responsive").shouldHave(text("Valeriy Samodurov"));
+            $(".table-responsive").shouldHave(text("valeron1337@test.ru"));
+            $(".table-responsive").shouldHave(text("Male"));
+            $(".table-responsive").shouldHave(text("9993274687"));
+            $(".table-responsive").shouldHave(text("19 July,1990"));
+            $(".table-responsive").shouldHave(text("Computer Science"));
+            $(".table-responsive").shouldHave(text("Reading"));
+            $(".table-responsive").shouldHave(text("Приложение.jpeg"));
+            $(".table-responsive").shouldHave(text("Sun street 12"));
+            $(".table-responsive").shouldHave(text("Uttar Pradesh Agra"));
+
+//закрываем таблицу
+            $("#closeLargeModal").click();
         }
     }
